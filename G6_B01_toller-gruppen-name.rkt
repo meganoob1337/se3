48dc05185bea2ac4d4f5ac237f6d5772118f2104
Arne Feil <git@arnefeil.de>
2013-11-03 18:09:51 +0100

    1.1 1.2 1.3 2.1

--- G6_B01_toller-gruppen-name.rkt
+++ G6_B01_toller-gruppen-name.rkt
@@ -1,4 +1,7 @@
 #lang racket
+;;;;;;;;;;;
+;; 1.1 ;;
+;;;;;;;;;;;
 ;;; rechnet gradmaß ins bogenmaß um
 (define (degrees->radians x) 
   (/ (* 2 pi x) 360)
@@ -7,4 +10,26 @@
 ;;; rechnet bogenmaß in gradmaß um
 (define (radians->degrees x)
   (/ (* 360 x) (* 2 pi))
+)
+
+;;;;;;;;;;;
+;; 1.2 ;;
+;;;;;;;;;;;
+(define (my-acos a)
+  (radians->degrees(atan (/ (sqrt (- 1 (* (cos a) (cos a))))
+          (cos a))))
+  )
+
+;;;;;;;;;;;
+;; 1.3 ;;
+;;;;;;;;;;;
+(define (nm->km l)
+  (* 1.852 l)
+)
+
+;;;;;;;;;;;
+;; 2.1 ;;
+;;;;;;;;;;;
+(define (distanzAB a1 a2 b1 b2) (
+                                 
 )
