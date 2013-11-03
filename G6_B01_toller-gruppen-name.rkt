5ffaa58edc24bbbe51b1c8c9cd7b79072773123d
Arne Feil <git@arnefeil.de>
2013-11-03 19:11:34 +0100

    2.2 fehlt

--- G6_B01_toller-gruppen-name.rkt
+++ G6_B01_toller-gruppen-name.rkt
@@ -11,11 +11,13 @@
 )
 
 ;;; Aufgabe 1.2
-;; berechnet den arccos eines winkels a
+;; berechnet den arccos eines cos
 (define (my-acos a)
-  (radians->degrees(atan (/ (sqrt (- 1 (* (cos a) (cos a))))
-          (cos a))))
-  )
+  (radians->degrees 
+   (atan (/ 
+          (sqrt (- 1 (*  a  a)))
+           a))
+   ))
 
 ;;; Aufgabe 1.3
 ;; rechnet seemeilen in kilometer um
@@ -28,9 +30,67 @@
 ; L : Geographische Länge
 (define (distanzAB Pa La Pb Lb)
   (nm->km (* 60 
-             (my-acos
+              (my-acos
                     (+
-                     (* (sin Pa) (sin Pb))
-                     (* (cos Pa) (cos Pb) (cos (- Lb La)))
+                     (* (sin (degrees->radians Pa)) 
+                         (sin (degrees->radians Pb)))
+                     (* (cos (degrees->radians Pa)) 
+                         (cos (degrees->radians Pb)) 
+                         (cos (degrees->radians(- Lb La))))
                     ))))
 )
+
+; distanz oslo hongkong
+(display "Oslo -> Hongkong\n")
+(distanzAB 59.93 10.75 22.20 114.10)
+; distanz san francisco honolulu
+(display "San Francisco->Honolulu\n")
+(distanzAB 37.75 -122.45 21.32 -157.83)
+;distanz osterinsel lima
+(display "Osterinsel->Lima\n")
+(distanzAB -27.10 -109.40 -12.10 -77.05)
+
+;;; Aufgabe 2.3
+;;; 2.3.1
+(define (grad->himmelsrichtung x)
+  (cond [(= x 0) "N"]
+            [(= x 22.5) "NNE"]
+            [(= x 45) "NE"]
+            [(= x 67.5) "ENE"]
+            [(= x 90) "E"]
+            [(= x 112.5) "ESE"]
+            [(= x 135) "SE"]
+            [(= x 157.5) "SSE"]
+            [(= x 180) "S"]
+            [(= x 202.5) "SSW"]
+            [(= x 225) "SW"]
+            [(= x 247.5) "WSW"]
+            [(= x 270) "W"]
+            [(= x 292.5) "WNW"]
+            [(= x 315) "NW"]
+            [(= x 337.5) "NNW"]
+            [else "not defined"]
+       )
+  )
+
+;; 2.3.2
+(define (himmelsrichtung->grad x)
+  (cond [(equal? x "N") 0]
+            [(equal? x "NNE") 22.5]
+            [(equal? x "NE") 45]
+            [(equal? x "ENE") 67.5]
+            [(equal? x "E") 90]
+            [(equal? x "ESE") 112.5]
+            [(equal? x "SE") 135]
+            [(equal? x "SSE") 157.5]
+            [(equal? x "S") 180]
+            [(equal? x "SSW") 202.5]
+            [(equal? x "SW") 225]
+            [(equal? x "WSW") 247.5]
+            [(equal? x "W") 270]
+            [(equal? x "WNW") 292.5]
+            [(equal? x "NW") 315]
+            [(equal? x "NNW") 337.5]
+            [else "not defined"]
+            )
+  )
